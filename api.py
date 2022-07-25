2a3e13504478560e20847d90908b9fa7f4b9f58e
rsuriano <ramirosuriano@gmail.com>
2022-07-24 23:28:17 -0300

    add: docs metadata

--- api.py
+++ api.py
@@ -7,7 +7,14 @@ from db_conn import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title='Ed I\\\\achina - Backend Challenge')
+app = FastAPI(\
+    title='Ed I\\\\achina - Backend Challenge',
+    version='1.0.0',
+    contact={
+        'name': 'Ramiro Suriano',
+        'url': 'https://github.com/rsuriano'
+        },
+    )
 
 # DB for dependency injection
 def get_db():
@@ -53,28 +60,14 @@ def create_enrollment(enrollment: schemas.EnrollmentCreate, db: Session = Depend
 def get_enrollments(db: Session = Depends(get_db)):
     return crud.get_enrollments(db)
 
-# @app.get("/enrollment/{student_id}", response_model=list[schemas.Enrollment])
-# def get_enrollment_by_student_id(student_id: int, db: Session = Depends(get_db)):
-#     db_enrollment = crud.get_enrollments_by_student_id(db, student_id=student_id)
-#     if db_enrollment is None:
-#         raise HTTPException(status_code=404, detail="Lead not found")
-#     return db_enrollment
-
 
 @app.post("/leads/", response_model=schemas.Lead, tags=["leads"])
 def create_lead(lead: schemas.LeadCreate, db: Session = Depends(get_db)):
-    db_lead = crud.create_lead(db, lead=lead)
-    if db_lead is None:
-        raise HTTPException(\
-            status_code=400, \
-            detail="Subject doesn't belong to any of the student's degrees.")
-    else:
-        return db_lead
+    return crud.create_lead(db, lead=lead)
 
 @app.get("/leads/", response_model=list[schemas.Lead], tags=["leads"])
 def get_leads(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    db_leads = crud.get_leads(db, skip=skip, limit=limit)
-    return db_leads
+    return get_leads(db, skip=skip, limit=limit)
 
 @app.get("/leads/{lead_id}", response_model=schemas.Lead, tags=["leads"])
 def get_lead_by_id(lead_id: int, db: Session = Depends(get_db)):

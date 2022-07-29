e2108c301ad43efdf46321cdf41830f4485ac166
rsuriano <ramirosuriano@gmail.com>
2022-07-29 02:47:57 -0300

    fix: misc fixes

--- backend/db_conn.py
+++ backend/db_conn.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 DB_URL = "postgresql://postgres:password@host.docker.internal/edmachina"
 
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
